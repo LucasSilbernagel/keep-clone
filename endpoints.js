223b5fa9100b02691f56718388efe7c4aa4eb007
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2022-07-15 22:04:26 -0400

    removed TypeScript from the back end

--- endpoints.js
+++ endpoints.js
@@ -1,23 +1,24 @@
-import express, { Request, Response } from 'express'
+/* eslint-disable @typescript-eslint/no-var-requires */
+const express = require('express')
 const router = express.Router()
-import Note from './models/Note'
+const Note = require('./models/Note')
 
 /** Return all notes for the authenticated user */
-router.get('/notes', async (req: Request, res: Response, next) => {
+router.get('/notes', async (req, res, next) => {
   await Note.find({ userGoogleId: req.query.userGoogleId })
-    .then((data: unknown) => res.json(data))
+    .then((data) => res.json(data))
     .catch(next)
 })
 
 /** Post a new note */
-router.post('/notes', async (req: Request, res: Response, next) => {
+router.post('/notes', async (req, res, next) => {
   await Note.create(req.body)
     .then((data) => res.json(data))
     .catch(next)
 })
 
 /** Edit a note with a specific ID */
-router.put('/notes/:id', async (req: Request, res: Response, next) => {
+router.put('/notes/:id', async (req, res, next) => {
   await Note.findByIdAndUpdate(req.params.id, req.body)
     .then(() => res.json({ msg: 'Updated successfully' }))
     .catch(next, () =>
@@ -26,24 +27,24 @@ router.put('/notes/:id', async (req: Request, res: Response, next) => {
 })
 
 /** Delete a note with a specific ID */
-router.delete('/notes/:id', async (req: Request, res: Response, next) => {
+router.delete('/notes/:id', async (req, res, next) => {
   await Note.findOneAndDelete({ _id: req.params.id })
-    .then((data: unknown) => res.json(data))
+    .then((data) => res.json(data))
     .catch(next)
 })
 
 /** Delete multiple notes by ID */
-router.post('/notes/batchDelete', async (req: Request, res: Response, next) => {
+router.post('/notes/batchDelete', async (req, res, next) => {
   const { ids } = req.body
   await Note.deleteMany({
     _id: { $in: ids },
   })
-    .then((data: unknown) => res.json(data))
+    .then((data) => res.json(data))
     .catch(next)
 })
 
 /** Edit multiple notes by ID */
-router.post('/notes/:editField', async (req: Request, res: Response, next) => {
+router.post('/notes/:editField', async (req, res, next) => {
   const { ids } = req.body
   const editField = req.params.editField
   if (editField === 'isPinned') {
@@ -51,9 +52,9 @@ router.post('/notes/:editField', async (req: Request, res: Response, next) => {
       { isPinned: false, _id: { $in: ids } },
       { $set: { isPinned: true } }
     )
-      .then((data: unknown) => res.json(data))
+      .then((data) => res.json(data))
       .catch(next)
   }
 })
 
-export { router as noteRouter }
+module.exports = router
--- models/Note.js
+++ models/Note.js
@@ -1,5 +1,6 @@
-import { model, Schema as _Schema } from 'mongoose'
-const Schema = _Schema
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const NoteListSchema = new Schema({
   text: String,
@@ -32,6 +33,6 @@ const NoteSchema = new Schema({
   },
 })
 
-const Note = model('Note', NoteSchema)
+const Note = mongoose.model('Note', NoteSchema)
 
-export default Note
+module.exports = Note
