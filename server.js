da3af62e73dc15216580d7b8e24ab76da2d82d3e
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2022-10-16 17:14:30 -0400

    another server change

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use((_req, res, next) => {
 app.use(bodyParser.json({ limit: '50mb' }))
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 app.use('/api', noteRouter)
-app.use(express.static(path.join(__dirname, 'client/build')))
+// app.use(express.static(path.join(__dirname, 'client/build')))
 app.get('*', (_req, res) => res.send('Server running'))
 app.use((err, _req, _res, next) => {
   console.error(err)
