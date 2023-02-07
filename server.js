73043d41c0e221d3eb6b28bd06529408a758e2ea
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2023-02-06 21:10:26 -0500

    Fixed CORS issue

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ mongoose
 mongoose.Promise = global.Promise
 app.use((_req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
